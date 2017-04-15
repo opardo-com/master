9a62c2ea13ca918c037e960f35c722773e6bfb6b
Hector Trujillo <hectortrujillo@Hectors-Mac-mini.local>
2017-04-14 22:11:03 -0400

    deleting unnecesary dirs

--- src/app/panel/panel.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'panel',
-  templateUrl: './app/panel/panel.component.html',
-  styleUrls: ['./app/panel/panel.component.css']
-})
-export class PanelComponent  { }
